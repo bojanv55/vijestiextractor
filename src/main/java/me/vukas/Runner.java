c25e56fceb032daa5cfe9ac350431ef77f440528
Bojan Vukasovic <b.vukasovic@sportradar.com>
2018-06-29 16:34:26 +0200

    updateovano

--- src/main/java/me/vukas/Runner.java
+++ src/main/java/me/vukas/Runner.java
@@ -1,16 +1,15 @@
 package me.vukas;
 
+import java.util.Optional;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 @Component
 public class Runner implements CommandLineRunner {
@@ -19,6 +18,8 @@ public class Runner implements CommandLineRunner {
     private ArticleRepository articleRepository;
     @Autowired
     private RijecRepository rijecRepository;
+
+	final private Pattern p = Pattern.compile("\\w+",Pattern.UNICODE_CHARACTER_CLASS);
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,12 +34,11 @@ public class Runner implements CommandLineRunner {
             pages = articleRepository.findAll(PageRequest.of(page, 10)); //fetch 10 records at once
 
             for (Article a : pages) {
-                String[] titleList = a.getTitle()
-                        .split(" ");
-                String[] contentList = a.getContent()
-                        .split(" ");
+							String[] titleList = p.matcher(a.getTitle()).results().map(MatchResult::group).toArray(String[]::new);
+							String[] contentList = p.matcher(a.getContent()).results().map(MatchResult::group).toArray(String[]::new);
 
-                String[] result = Stream.of(titleList, contentList).flatMap(Stream::of).toArray(String[]::new);
+
+							String[] result = Stream.of(titleList, contentList).flatMap(Stream::of).toArray(String[]::new);
 
                 for (String ts1 : result) {
                     String ts = ts1.toLowerCase();
