5dcba7c6ae6c75989256d3c6e34f18314c41fc77
Bojan Vukasovic <bojan@vukas.me>
2018-07-07 00:44:36 +0200

    webflux

---
+++ src/main/java/me/vukas/webflux/App.java
@@ -0,0 +1,25 @@
+package me.vukas.webflux;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(App.class)
+                .bannerMode(Banner.Mode.OFF)
+                //.web(WebApplicationType.NONE)
+                .build()
+                .run(args);
+    }
+}
---
+++ src/main/java/me/vukas/webflux/Runner.java
@@ -0,0 +1,106 @@
+package me.vukas.webflux;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Component
+public class Runner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        Mono.just("Craig")
+                .map(String::toUpperCase)
+                .map(x -> "Cao " + x)
+                .subscribe(System.out::println);
+
+        Flux<String> ffs = Flux.just("a", "b", "c");
+
+        //koristi se za testiranje
+//        StepVerifier.create(ffs)
+//                .expectNext("a")
+//                .expectNext("b")
+//                .expectNext("c")
+//                .verifyComplete();
+
+        //ffs = Flux.fromIterable(Arrays.asList("1", "2", "3"));
+        ffs = Flux.fromStream(Stream.of("1", "2", "3", "4"));
+
+        ffs.subscribe(f -> System.out.println("pr: " + f));
+
+
+        Flux.range(1,50).subscribe(System.out::println);
+
+        //mora biti webapp ili sleep jer je ocigledno neki deamon thread
+        //svake 2 sekunde emituje elemente od 0..9
+        //Flux.interval(Duration.ofSeconds(2)).take(10).subscribe(System.out::println);
+
+        //merge 2 fluxs
+        Flux<Integer> fi1 = Flux.fromStream(Stream.iterate(99, x -> x+1).limit(10))
+                .delayElements(Duration.ofMillis(500));
+        Flux<Integer> fi2 = Flux.fromStream(Stream.iterate(120, x -> x+1).limit(10))
+                .delaySubscription(Duration.ofMillis(250)) //nemoj odma da emitujes poslije subscr
+                .delayElements(Duration.ofMillis(500)); //emitovanje je odlozeno za po 500 ms
+
+        fi1.mergeWith(fi2).subscribe(System.out::println);
+
+        Flux<Integer> fi21 = Flux.range(99,10);
+        Flux<Integer> fi22 = Flux.range(120,10);
+
+        Flux.zip(fi21, fi22).subscribe(System.out::println); // [99, 120], [100,121], ...
+        //flux se ne trosi kao Stream (koji moze samo 1)
+        Flux.zip(fi21, fi22, (a,b) -> a + " + " + b).subscribe(System.out::println); // 99+120...
+
+        //odabere brzi od 2 fluxa
+        Flux.first(fi21, fi22.delaySubscription(Duration.ofMillis(100)))
+                .subscribe(System.out::println);    //samo ce fi21 da se publishuje jer je drugi spor
+
+
+        //preskace prvih 10, pa svaki sledeci stampa po 1 sekund a dodatno preskoci jos 4 zbog duration skipa
+        //take ogranicava na 20 elemenata poslije skipa
+        Flux.range(1,100).skip(10).take(20).delayElements(Duration.ofSeconds(1))
+                .skip(Duration.ofSeconds(5)).subscribe(System.out::println);
+
+        Flux.just("e","r","z","r","r","g")
+                .distinct() //samo jedno r
+                .filter(x -> x.equals("r")) //samo r
+                .map(x -> "|"+x+"|")    //garantuje redosljed
+                .subscribe(System.out::println);
+
+        //mapping moze da se odradi asynch i parallelno? Nije garantovan redosljed
+        Flux.just("a", "b", "c", "d", "e", "f", "g") //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                .flatMap(n -> Mono.just(n)
+                .map(f -> f.toUpperCase()).subscribeOn(Schedulers.parallel())
+                ).subscribe(System.out::println);
+
+
+        //grupise u liste od po 3
+        Flux.just("a", "b", "c", "d", "e", "f", "g").buffer(3)
+                .subscribe(System.out::println);    // [a,b,c], [d,e,f] ...
+
+        Flux.just("x", "y", "z", "y", "j", "r", "d").buffer(2)
+                .flatMap(x ->
+                        Flux.fromIterable(x)    //svaki array koji je bufferovan se obradjuje u novom tredu
+                        .map(y -> y.toUpperCase())
+                        .subscribeOn(Schedulers.parallel())
+                        .log()  //prikazuje se u log-u
+                ).subscribe();
+
+
+        Mono<List<String>> mls = Flux.just("x", "y", "z", "y", "j", "r", "d").collectList();
+        Mono<Map<String, String>> mmf = Flux.just("x", "y", "z", "y", "j", "r", "d").collectMap(k -> k.toUpperCase());
+
+
+        Flux.just("x", "y", "z", "y", "j", "r", "d").any(x -> x.equals("x")).subscribe(System.out::println);
+        Flux.just("x", "y", "z", "y", "j", "r", "d").any(x -> x.equals("g")).subscribe(System.out::println);
+    }
+}
